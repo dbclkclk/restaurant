c21694eba75f2c2ceb891cb7930f222ffcd6328f
Dean Clarke <deanclarke@Deans-MacBook-Pro.local>
2016-12-16 18:53:30 -0400

    development configuration

--- api/config/connection.js
+++ api/config/connection.js
@@ -9,11 +9,11 @@ module.exports.connections = {
   connections: {
     mongoConnection: {
       adapter: 'mongodb',
-      host: process.env.MONGO_HOST,
-      port:process.env.MONGO_PORT,
-      database: process.env.MONGO_DB,
-      user: process.env.MONGO_USER,
-      password: process.env.MONGO_PASSWORD
+      host: process.env.MONGO_HOST ? process.env.MONGO_HOST : "database",
+      port:process.env.MONGO_PORT ? process.env.MONGO_PORT : 27017,
+      database: process.env.MONGO_DB ? process.env.MONGO_DB : "test",
+      user: process.env.MONGO_USER ? process.env.MONGO_USER : "",
+      password: process.env.MONGO_PASSWORD ? process.env.MONGO_PASSWORD : ""
     }
   }
 };
