ab246674de86f35c987e0c2b77e01ad40fa16985
Dean Clarke <deanclarke@Deans-MacBook-Pro.local>
2016-12-16 23:18:49 -0400

    prod load data

--- api/data/import_prod.sh
+++ api/data/import_prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "import allergies"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy -u <user> -p <password> --file /api/data/allergies.json --jsonArray"
+eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy  --drop -u <user> -p password --file /api/data/allergies.json --jsonArray"
 echo "importing products"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c product -u admin -p p@ssw0rd --file /api/data/products.json --jsonArray"
+eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c product --drop -u admin -p password --file /api/data/products.json --jsonArray"
 echo "importing products_allergies"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy_products__product_allergies -u admin -p p@ssw0rd --file /api/data/product_allergy.json --jsonArray"
+eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy_products__product_allergies --drop -u admin -p password --file /api/data/product_allergy.json --jsonArray"
 
