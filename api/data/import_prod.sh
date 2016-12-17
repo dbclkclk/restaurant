89b7d896e169118c95a7137ebf4df4e1494c4684
Dean Clarke <deanclarke@Deans-MacBook-Pro.local>
2016-12-17 09:22:33 -0400

    modify for heroku

--- api/data/import_prod.sh
+++ api/data/import_prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "import allergies"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy  --drop -u <user> -p password --file /api/data/allergies.json --jsonArray"
+eval "/usr/bin/mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy  --drop -u admin -p password --file /dev/shm/allergies.json --jsonArray"
 echo "importing products"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c product --drop -u admin -p password --file /api/data/products.json --jsonArray"
+eval "/usr/bin/mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c product --drop -u admin -p password --file /dev/shm/products.json --jsonArray"
 echo "importing products_allergies"
-eval "mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy_products__product_allergies --drop -u admin -p password --file /api/data/product_allergy.json --jsonArray"
+eval "/usr/bin/mongoimport -h ds023500.mlab.com:23500 -d medullan_test -c allergy_products__product_allergies --drop -u admin -p password --file /dev/shm/product_allergy.json --jsonArray"
 
